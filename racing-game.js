7b8ded507fc822f82abdebc9c2744c57a02b3549
elijah-atkins <elijahorama@gmail.com>
2020-02-02 10:35:29 -0700

    continueing tutorial on racing game

--- racing-game.js
+++ racing-game.js
@@ -1,22 +1,28 @@
-var ballX = 400;
-var ballSpeedX = 0;
-var ballY = 500;
-var ballSpeedY = 5;
-const BG_COLOR = 'black';
-const BALL_COLOR = 'green';
-const TRACK_COLOR = 'green';
+var ballX = 75;
+var ballY = 75;
+var ballSpeedX = 5;
+var ballSpeedY = 7;
 
-const TRACK_W = 80;
+const TRACK_W = 40;
 const TRACK_H = 40;
-const TRACK_COL = 10;
-const TRACK_ROWS = 9;
 const TRACK_GAP = 2;
-
-var trackGrid = new Array(TRACK_COL * TRACK_ROWS);
-var tracksLeft = 0;
-
-
-
+const TRACK_COLS = 20;
+const TRACK_ROWS = 15;
+var trackGrid = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+                 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+                 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+                 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+                 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+                 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+                 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+                 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+                 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+                 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+                 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+                 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+                 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+                 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+                 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
 
 var canvas, canvasContext;
 
@@ -24,194 +30,158 @@ var mouseX = 0;
 var mouseY = 0;
 
 function updateMousePos(evt) {
-  var rect = canvas.getBoundingClientRect();
-  var root = document.documentElement;
+	var rect = canvas.getBoundingClientRect();
+	var root = document.documentElement;
 
-  mouseX = evt.clientX - rect.left - root.scrollLeft;
-  mouseY = evt.clientY - rect.top - root.scrollTop;
+	mouseX = evt.clientX - rect.left - root.scrollLeft;
+	mouseY = evt.clientY - rect.top - root.scrollTop;
 
-
-  /* mouse cheat
-  ballX = mouseX;
-  ballY = mouseY;
-  ballSpeedX = 4;
-  ballSpeedY = -4;
-  */
+	// cheat / hack to test ball in any position
+	/*ballX = mouseX;
+	ballY = mouseY;
+	ballSpeedX = 4;
+	ballSpeedY = -4;*/
 }
-function trackReset() {
-  tracksLeft = 0;
-  var i;
-  for(i=0;i<3 * TRACK_COL;i++){
-    trackGrid[i] = false;
-  }
-  for (; i < TRACK_COL * TRACK_ROWS; i++) {
-    trackGrid[i] = true;
-    tracksLeft++;
-  } // end of for each track
-} //end of trackReset
+
+
 window.onload = function() {
-  canvas = document.getElementById("gameCanvas");
-  canvasContext = canvas.getContext("2d");
-  canvasContext.font="bold 36px 'Zilla Slab";
+	canvas = document.getElementById('gameCanvas');
+	canvasContext = canvas.getContext('2d');
 
-  var framesPerSecond = 30;
-  setInterval(updateAll, 1000 / framesPerSecond);
+	var framesPerSecond = 30;
+	setInterval(updateAll, 1000/framesPerSecond);
 
-  canvas.addEventListener("mousemove", updateMousePos);
+	canvas.addEventListener('mousemove', updateMousePos);
 
-  trackReset();
-};
+	trackReset();
+	ballReset();
+}
+
 function updateAll() {
-  moveAll();
-
-  drawAll();
+	moveAll();
+	drawAll();
 }
+
 function ballReset() {
-  ballX = canvas.width / 2;
-  ballY = canvas.height *.75;
+	ballX = canvas.width/2;
+	ballY = canvas.height/2;
 }
 
-function ballMove(){
-  ballX += ballSpeedX;
-  ballY += ballSpeedY;
-  if (ballX > canvas.width && ballSpeedX > 0.0) {
-    // right
-    ballSpeedX *= -1;
+function ballMove() {
+	ballX += ballSpeedX;
+	ballY += ballSpeedY;
 
-  }
-  if (ballX < 0 && ballSpeedX < 0.0) {
-    //left
-    ballSpeedX *= -1;
-
-  }
-  if (ballY < 0 && ballSpeedY < 0.0) {
-    // top
-    ballSpeedY *= -1;
-
-  }
-  if (ballY > canvas.height) {
-    //bottom
-    ballSpeedX = 0;
-    ballReset();
-    trackReset();
-  
+	if(ballX < 0 && ballSpeedX < 0.0) { //left
+		ballSpeedX *= -1;
+	}
+	if(ballX > canvas.width && ballSpeedX > 0.0) { // right
+		ballSpeedX *= -1;
+	}
+	if(ballY < 0 && ballSpeedY < 0.0) { // top
+		ballSpeedY *= -1;
+	}
+	if(ballY > canvas.height) { // bottom
+		ballReset();
+		trackReset();
+	}
 }
-function isTRACKAtColRow(col,row){
-  if (
-    col >= 0 && col < TRACK_COL &&
-    row >= 0 && row < TRACK_ROWS) {
-      var trackIndexUnderCoord = rowColToArrayIndex(col,row);
-      return trackGrid[trackIndexUnderCoord];
-    } else {
-      return false;
-    }
+
+function isTrackAtColRow(col, row) {
+	if(col >= 0 && col < TRACK_COLS &&
+		row >= 0 && row < TRACK_ROWS) {
+		 var trackIndexUnderCoord = rowColToArrayIndex(col, row);
+		 return trackGrid[trackIndexUnderCoord];
+	} else {
+		return false;
+	}
 }
-function ballTRACKHandling(){
-  var ballTRACKCol = Math.floor(ballX / TRACK_W);
-  var ballTRACKRow = Math.floor(ballY / TRACK_H);
-  var trackIndexUnderBall = rowColToArrayIndex(
-    ballTRACKCol,
-    ballTRACKRow
-  );
-  if (
-    ballTRACKCol >= 0 &&
-    ballTRACKCol < TRACK_COL &&
-    ballTRACKRow >= 0 &&
-    ballTRACKRow < TRACK_ROWS
-  ) {
-    if (isTRACKAtColRow( ballTRACKCol, ballTRACKRow )) {
-      trackGrid[trackIndexUnderBall] = false;
-      tracksLeft--;
-      //console.log(tracksLeft);
 
-      var prevBallX = ballX - ballSpeedX;
-      var prevBallY = ballY - ballSpeedY;
-      var prevTRACKCol = Math.floor(prevBallX / TRACK_W);
-      var prevTRACKRow = Math.floor(prevBallY / TRACK_H);
-      
-      var bothTestsFailed = true;
+function ballTrackHandling() {
+	var ballTrackCol = Math.floor(ballX / TRACK_W);
+	var ballTrackRow = Math.floor(ballY / TRACK_H);
+	var trackIndexUnderBall = rowColToArrayIndex(ballTrackCol, ballTrackRow);
 
-      if(prevTRACKCol != ballTRACKCol){
-        if(isTRACKAtColRow( prevTRACKCol, ballTRACKRow ) == false) {
-          ballSpeedX *= -1;
-          bothTestFailed = false;
-        }
-      }
-      if(prevTRACKRow != ballTRACKRow){
-        if(isTRACKAtColRow( ballTRACKCol, prevTRACKRow )){
-          ballSpeedY *= -1;
-          bothTestFailed = false;
-        }
-        if(bothTestsFailed){//armpitcase prevents ball from going through diagnol
-          ballSpeedY *= -1;
-          ballSpeedX *= -1;
-        }
-      }
-    } // end of trackfound
-  }// end of valid col and row
-}//end of function ballTRACKHandling
+	if(ballTrackCol >= 0 && ballTrackCol < TRACK_COLS &&
+		ballTrackRow >= 0 && ballTrackRow < TRACK_ROWS) {
 
+		if(isTrackAtColRow( ballTrackCol,ballTrackRow )) {
 
+			var prevBallX = ballX - ballSpeedX;
+			var prevBallY = ballY - ballSpeedY;
+			var prevTrackCol = Math.floor(prevBallX / TRACK_W);
+			var prevTrackRow = Math.floor(prevBallY / TRACK_H);
+
+			var bothTestsFailed = true;
+
+			if(prevTrackCol != ballTrackCol) {
+				if(isTrackAtColRow(prevTrackCol, ballTrackRow) == false) {
+					ballSpeedX *= -1;
+					bothTestsFailed = false;
+				}
+			}
+			if(prevTrackRow != ballTrackRow) {
+				if(isTrackAtColRow(ballTrackCol, prevTrackRow) == false) {
+					ballSpeedY *= -1;
+					bothTestsFailed = false;
+				}
+			}
+
+			if(bothTestsFailed) { // armpit case, prevents ball from going through
+				ballSpeedX *= -1;
+				ballSpeedY *= -1;
+			}
+
+		} // end of track found
+	} // end of valid col and row
+} // end of ballTrackHandling func
 
 function moveAll() {
-  ballMove();
-
-  ballTRACKHandling();
-
-
+	ballMove();
+	
+	ballTrackHandling();
 }
 
 function rowColToArrayIndex(col, row) {
-  return col + TRACK_COL * row;
+	return col + TRACK_COLS * row;
 }
 
-function drawTRACKs() {
-  for (let eachRow = 0; eachRow < TRACK_ROWS; eachRow++) {
-    for (let eachCol = 0; eachCol < TRACK_COL; eachCol++) {
-      var arrayIndex = rowColToArrayIndex(eachCol, eachRow);
+function drawTracks() {
 
-      if (trackGrid[arrayIndex]) {
-        colorRect(
-          TRACK_W * eachCol,
-          TRACK_H * eachRow,
-          TRACK_W - TRACK_GAP,
-          TRACK_H - TRACK_GAP,
-          TRACK_COLOR
-        );
-      } // end of if
-    } // end of for
-  } //end of for each row
-} // end of function
+	for(var eachRow=0;eachRow<TRACK_ROWS;eachRow++) {
+		for(var eachCol=0;eachCol<TRACK_COLS;eachCol++) {
+
+			var arrayIndex = rowColToArrayIndex(eachCol, eachRow); 
+
+			if(trackGrid[arrayIndex]) {
+				colorRect(TRACK_W*eachCol,TRACK_H*eachRow,
+					TRACK_W-TRACK_GAP,TRACK_H-TRACK_GAP, 'blue');
+			} // end of is this track here
+		} // end of for each track
+	} // end of for each row
+
+} // end of drawTracks func
+
 function drawAll() {
-  //clear screen
-  colorRect(0, 0, canvas.width, canvas.height, BG_COLOR);
+	colorRect(0,0, canvas.width,canvas.height, 'black'); // clear screen
 
-  
-  drawTRACKs();
-  //draw ball
-  colorCircle(ballX, ballY, 10, BALL_COLOR);
-  //draw mouse cordinates
-  /*
-  var mouseTRACKCol = Math.floor(mouseX / TRACK_W);
-  var mouseTRACKRow = Math.floor(mouseY / TRACK_H);
-  var trackIndexUnderMouse = rowColToArrayIndex(mouseTRACKCol, mouseTRACKRow);
-  colorText(mouseTRACKCol + "," + mouseTRACKRow+':'+trackIndexUnderMouse, mouseX, mouseY, "yellow"); 
-  */
-  }
+	colorCircle(ballX,ballY, 10, 'white'); // draw ball
 
-
-function colorRect(topLeftX, topLeftY, boxWidth, boxHeight, fillColor) {
-  canvasContext.fillStyle = fillColor;
-  canvasContext.fillRect(topLeftX, topLeftY, boxWidth, boxHeight);
+	drawTracks();
 }
-function colorCircle(centerX, centerY, radius, fillColor) {
-  canvasContext.fillStyle = fillColor;
-  canvasContext.beginPath();
 
-  canvasContext.arc(centerX, centerY, radius, 0, Math.PI * 2, true);
-  canvasContext.fill();
+function colorRect(topLeftX,topLeftY, boxWidth,boxHeight, fillColor) {
+	canvasContext.fillStyle = fillColor;
+	canvasContext.fillRect(topLeftX,topLeftY, boxWidth,boxHeight);
 }
-function colorText(showWords, textX, textY, fillColor) {
-  canvasContext.fillStyle = fillColor;
-  canvasContext.fillText(showWords, textX, textY);
+
+function colorCircle(centerX,centerY, radius, fillColor) {
+	canvasContext.fillStyle = fillColor;
+	canvasContext.beginPath();
+	canvasContext.arc(centerX,centerY, 10, 0,Math.PI*2, true);
+	canvasContext.fill();
+}
+
+function colorText(showWords, textX,textY, fillColor) {
+	canvasContext.fillStyle = fillColor;
+	canvasContext.fillText(showWords, textX, textY);
 }
