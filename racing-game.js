40c705a8962223b155828acbf68d30cc615b5405
elijah-atkins <elijahorama@gmail.com>
2020-02-02 15:29:08 -0700

    added car graphic to code

--- racing-game.js
+++ racing-game.js
@@ -1,8 +1,11 @@
-var ballX =75;
-var ballY =75;
+var carPic = document.createElement("img");
+var carPicLoaded = false;
+
+var ballX = 75;
+var ballY = 75;
 var ballSpeedX = 5;
 var ballSpeedY = 7;
-const BALL_COLOR = 'white'
+
 const TRACK_COLOR = 'blue'
 const BG_COLOR = 'black'
 const TRACK_W = 40;
@@ -10,21 +13,22 @@ const TRACK_H = 40;
 const TRACK_GAP = 2;
 const TRACK_COLS = 20;
 const TRACK_ROWS = 15;
-var trackGrid = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-                 1,1,1,1,1,1,0,0,0,0,0,0,0,1,1,1,1,1,1,1,
-                 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,
-                 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,
-                 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,
-                 1,0,0,0,0,0,0,0,1,1,0,1,0,0,0,0,0,0,1,1,
-                 1,0,2,0,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,1,
-                 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,1,
-                 1,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,1,
-                 1,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,1,
-                 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-                 1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,
-                 1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,
-                 1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,
-                 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
+var trackGrid = 
+   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+	1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1,
+	1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1,
+	1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1,
+	1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
+	1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1,
+	1, 0, 2, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1,
+	1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1,
+	1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1,
+	1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
+	1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1,
+	1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1,
+	1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1,
+	1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1,
+	1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
 
 var canvas, canvasContext;
 
@@ -46,17 +50,21 @@ function updateMousePos(evt) {
 }
 
 
-window.onload = function() {
+window.onload = function () {
 	canvas = document.getElementById('gameCanvas');
 	canvasContext = canvas.getContext('2d');
 
 	var framesPerSecond = 30;
-	setInterval(updateAll, 1000/framesPerSecond);
+	setInterval(updateAll, 1000 / framesPerSecond);
 
 	canvas.addEventListener('mousemove', updateMousePos);
-  ballReset();
-	trackReset();
-	
+
+	carPic.onload = function () {
+		carPicLoaded = true;
+	}
+	carPic.src = "img/player1car.png";
+
+	ballReset();
 }
 
 function updateAll() {
@@ -65,13 +73,13 @@ function updateAll() {
 }
 
 function ballReset() {
-	for(var eachRow=0;eachRow<TRACK_ROWS;eachRow++) {
-		for(var eachCol=0;eachCol<TRACK_COLS;eachCol++) {
-			var arrayIndex = rowColToArrayIndex(eachCol, eachRow); 
-			if(trackGrid[arrayIndex] == 2) {
+	for (var eachRow = 0; eachRow < TRACK_ROWS; eachRow++) {
+		for (var eachCol = 0; eachCol < TRACK_COLS; eachCol++) {
+			var arrayIndex = rowColToArrayIndex(eachCol, eachRow);
+			if (trackGrid[arrayIndex] == 2) {
 				trackGrid[arrayIndex] = 0;
-				ballX = eachCol * TRACK_W + TRACK_W/2;
-				ballY = eachRow * TRACK_H + TRACK_H/2;
+				ballX = eachCol * TRACK_W + TRACK_W / 2;
+				ballY = eachRow * TRACK_H + TRACK_H / 2;
 			}
 		}
 	}
@@ -81,26 +89,26 @@ function ballMove() {
 	ballX += ballSpeedX;
 	ballY += ballSpeedY;
 
-	if(ballX < 0 && ballSpeedX < 0.0) { //left
+	if (ballX < 0 && ballSpeedX < 0.0) { //left
 		ballSpeedX *= -1;
 	}
-	if(ballX > canvas.width && ballSpeedX > 0.0) { // right
+	if (ballX > canvas.width && ballSpeedX > 0.0) { // right
 		ballSpeedX *= -1;
 	}
-	if(ballY < 0 && ballSpeedY < 0.0) { // top
+	if (ballY < 0 && ballSpeedY < 0.0) { // top
 		ballSpeedY *= -1;
 	}
-	if(ballY > canvas.height) { // bottom
+	if (ballY > canvas.height) { // bottom
 		ballReset();
-		trackReset();
+
 	}
 }
 
 function isTrackAtColRow(col, row) {
-	if(col >= 0 && col < TRACK_COLS &&
+	if (col >= 0 && col < TRACK_COLS &&
 		row >= 0 && row < TRACK_ROWS) {
-		 var trackIndexUnderCoord = rowColToArrayIndex(col, row);
-		 return trackGrid[trackIndexUnderCoord];
+		var trackIndexUnderCoord = rowColToArrayIndex(col, row);
+		return trackGrid[trackIndexUnderCoord];
 	} else {
 		return false;
 	}
@@ -111,10 +119,10 @@ function ballTrackHandling() {
 	var ballTrackRow = Math.floor(ballY / TRACK_H);
 	var trackIndexUnderBall = rowColToArrayIndex(ballTrackCol, ballTrackRow);
 
-	if(ballTrackCol >= 0 && ballTrackCol < TRACK_COLS &&
+	if (ballTrackCol >= 0 && ballTrackCol < TRACK_COLS &&
 		ballTrackRow >= 0 && ballTrackRow < TRACK_ROWS) {
 
-		if(isTrackAtColRow( ballTrackCol,ballTrackRow )) {
+		if (isTrackAtColRow(ballTrackCol, ballTrackRow)) {
 
 			var prevBallX = ballX - ballSpeedX;
 			var prevBallY = ballY - ballSpeedY;
@@ -123,20 +131,20 @@ function ballTrackHandling() {
 
 			var bothTestsFailed = true;
 
-			if(prevTrackCol != ballTrackCol) {
-				if(isTrackAtColRow(prevTrackCol, ballTrackRow) == false) {
+			if (prevTrackCol != ballTrackCol) {
+				if (isTrackAtColRow(prevTrackCol, ballTrackRow) == false) {
 					ballSpeedX *= -1;
 					bothTestsFailed = false;
 				}
 			}
-			if(prevTrackRow != ballTrackRow) {
-				if(isTrackAtColRow(ballTrackCol, prevTrackRow) == false) {
+			if (prevTrackRow != ballTrackRow) {
+				if (isTrackAtColRow(ballTrackCol, prevTrackRow) == false) {
 					ballSpeedY *= -1;
 					bothTestsFailed = false;
 				}
 			}
 
-			if(bothTestsFailed) { // armpit case, prevents ball from going through
+			if (bothTestsFailed) { // armpit case, prevents ball from going through
 				ballSpeedX *= -1;
 				ballSpeedY *= -1;
 			}
@@ -147,7 +155,7 @@ function ballTrackHandling() {
 
 function moveAll() {
 	ballMove();
-	
+
 	ballTrackHandling();
 }
 
@@ -157,14 +165,14 @@ function rowColToArrayIndex(col, row) {
 
 function drawTracks() {
 
-	for(var eachRow=0;eachRow<TRACK_ROWS;eachRow++) {
-		for(var eachCol=0;eachCol<TRACK_COLS;eachCol++) {
+	for (var eachRow = 0; eachRow < TRACK_ROWS; eachRow++) {
+		for (var eachCol = 0; eachCol < TRACK_COLS; eachCol++) {
 
-			var arrayIndex = rowColToArrayIndex(eachCol, eachRow); 
+			var arrayIndex = rowColToArrayIndex(eachCol, eachRow);
 
-			if(trackGrid[arrayIndex] == 1) {
-				colorRect(TRACK_W*eachCol,TRACK_H*eachRow,
-					TRACK_W-TRACK_GAP,TRACK_H-TRACK_GAP, TRACK_COLOR);
+			if (trackGrid[arrayIndex] == 1) {
+				colorRect(TRACK_W * eachCol, TRACK_H * eachRow,
+					TRACK_W - TRACK_GAP, TRACK_H - TRACK_GAP, TRACK_COLOR);
 			} // end of is this track here
 		} // end of for each track
 	} // end of for each row
@@ -172,26 +180,30 @@ function drawTracks() {
 } // end of drawTracks func
 
 function drawAll() {
-	colorRect(0,0, canvas.width,canvas.height, BG_COLOR); // clear screen
-
-	colorCircle(ballX,ballY, 10, BALL_COLOR); // draw ball
-
+	colorRect(0, 0, canvas.width, canvas.height, BG_COLOR); // clear screen
 	drawTracks();
+
+	//colorCircle(ballX,ballY, 10, BALL_COLOR); // draw ball
+	if (carPicLoaded) {
+		canvasContext.drawImage(carPic,
+			ballX - carPic.width / 2,
+			ballY - carPic.height / 2)
+	}
 }
 
-function colorRect(topLeftX,topLeftY, boxWidth,boxHeight, fillColor) {
+function colorRect(topLeftX, topLeftY, boxWidth, boxHeight, fillColor) {
 	canvasContext.fillStyle = fillColor;
-	canvasContext.fillRect(topLeftX,topLeftY, boxWidth,boxHeight);
+	canvasContext.fillRect(topLeftX, topLeftY, boxWidth, boxHeight);
 }
 
-function colorCircle(centerX,centerY, radius, fillColor) {
+function colorCircle(centerX, centerY, radius, fillColor) {
 	canvasContext.fillStyle = fillColor;
 	canvasContext.beginPath();
-	canvasContext.arc(centerX,centerY, 10, 0,Math.PI*2, true);
+	canvasContext.arc(centerX, centerY, 10, 0, Math.PI * 2, true);
 	canvasContext.fill();
 }
 
-function colorText(showWords, textX,textY, fillColor) {
+function colorText(showWords, textX, textY, fillColor) {
 	canvasContext.fillStyle = fillColor;
 	canvasContext.fillText(showWords, textX, textY);
 }
