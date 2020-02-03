ed8295ab6f0fe55a112d01717b5361efd4f8a9b2
elijah-atkins <elijahorama@gmail.com>
2020-02-03 10:41:12 -0700

    tuned car handling and cleaned up code

--- racing-game.js
+++ racing-game.js
@@ -6,12 +6,12 @@ var carY = 75;
 var carAng = 0;
 var carSpeed = 0;
 
-const GROUNDSPEED_DECAY_MULT = 0.96;
-const DRIVE_POWER = 0.6;
-const REVERSE_POWER = 0.5;
-const TURN_RATE = 0.05;
+const GROUNDSPEED_DECAY_MULT = 0.94;
+const DRIVE_POWER = 0.8;
+const REVERSE_POWER = 0.6;
+const TURN_RATE = .01;
 const SPEED_LIMIT = 8;
-const BREAK_LIMIT = -0.1;
+const BREAK_LIMIT = -2;
 
 const TRACK_COLOR = 'blue'
 const BG_COLOR = 'black'
@@ -36,6 +36,10 @@ var trackGrid =
 	1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1,
 	1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1,
 	1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+
+const TRACK_ROAD = 0;
+const TRACK_WALL = 1;
+const TRACK_PLAYERSTART = 2;
 
 var canvas, canvasContext;
 
@@ -128,8 +132,8 @@ function carReset() {
 	for (var eachRow = 0; eachRow < TRACK_ROWS; eachRow++) {
 		for (var eachCol = 0; eachCol < TRACK_COLS; eachCol++) {
 			var arrayIndex = rowColToArrayIndex(eachCol, eachRow);
-			if (trackGrid[arrayIndex] == 2) {
-				trackGrid[arrayIndex] = 0;
+			if (trackGrid[arrayIndex] == TRACK_PLAYERSTART) {
+				trackGrid[arrayIndex] = TRACK_ROAD;
 				carAng = -Math.PI/2;
 				carX = eachCol * TRACK_W + TRACK_W / 2;
 				carY = eachRow * TRACK_H + TRACK_H / 2;
@@ -147,10 +151,10 @@ function carMove() {
 		carSpeed -= REVERSE_POWER;
 	}
 	if(keyHeld_TurnLeft) {
-		carAng -= TURN_RATE;
+		carAng -= TURN_RATE * carSpeed;
 	}
 	if(keyHeld_TurnRight) {
-		carAng += TURN_RATE;
+		carAng += TURN_RATE * carSpeed;
 	}
 	carX += Math.cos(carAng) * carSpeed;
 	carY += Math.sin(carAng) * carSpeed;
@@ -160,7 +164,7 @@ function carMove() {
 
 }
 
-function isTrackAtColRow(col, row) {
+function isWallAtColRow(col, row) {
 	if (col >= 0 && col < TRACK_COLS &&
 		row >= 0 && row < TRACK_ROWS) {
 		var trackIndexUnderCoord = rowColToArrayIndex(col, row);
@@ -178,7 +182,7 @@ function carTrackHandling() {
 	if (carTrackCol >= 0 && carTrackCol < TRACK_COLS &&
 		carTrackRow >= 0 && carTrackRow < TRACK_ROWS) {
 
-		if (isTrackAtColRow(carTrackCol, carTrackRow)) {
+		if (isWallAtColRow(carTrackCol, carTrackRow)) {
 			carX -= Math.cos(carAng)* carSpeed;
 			carY -= Math.sin(carAng)* carSpeed;
 
@@ -205,7 +209,7 @@ function drawTracks() {
 
 			var arrayIndex = rowColToArrayIndex(eachCol, eachRow);
 
-			if (trackGrid[arrayIndex] == 1) {
+			if (trackGrid[arrayIndex] == TRACK_WALL) {
 				colorRect(TRACK_W * eachCol, TRACK_H * eachRow,
 					TRACK_W - TRACK_GAP, TRACK_H - TRACK_GAP, TRACK_COLOR);
 			} // end of is this track here
