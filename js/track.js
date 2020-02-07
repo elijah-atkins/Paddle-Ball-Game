a884d6aa1a200fad6ea73e14b5d8e2a38b4de27c
elijah-atkins <elijahorama@gmail.com>
2020-02-07 15:55:39 -0700

    added different tile tester

--- js/track.js
+++ js/track.js
@@ -18,23 +18,23 @@ const TRACK_ROWS = 15;
 // 				0, 3, 0, 0, 0, 0, 1, 4, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1,
 // 				0, 3, 0, 0, 0, 0, 1, 4, 4, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1,
 // 				1, 1, 1, 1, 1, 1, 1, 4, 4, 4, 4, 4, 4, 4, 1, 1, 1, 1, 1, 4];
-				
-var trackGrid = 
-   [4, 4, 4, 4, 4, 4, 1, 1, 1, 1, 1, 1, 1, 4, 4, 4, 4, 4, 4, 4,
-	4, 4, 1, 1, 1, 5, 0, 0, 0, 0, 0, 0, 0, 1, 1, 4, 4, 4, 4, 4,
-	4, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 4, 4, 4,
-	4, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 4, 4,
-	1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 4,
-	1, 0, 0, 0, 0, 5, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1,
-	1, 2, 2, 0, 5, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 0, 0, 0, 1, 1,
-	1, 1, 1, 1, 1, 1, 4, 4, 4, 4, 4, 4, 5, 5, 0, 0, 0, 0, 0, 1,
-	1, 0, 3, 0, 0, 0, 5, 4, 4, 4, 4, 5, 0, 0, 0, 0, 0, 0, 0, 1,
-	1, 0, 0, 0, 0, 0, 0, 5, 1, 1, 5, 0, 0, 0, 0, 0, 0, 0, 1, 1,
-	1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 1, 4,
-	4, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 4, 4,
-	4, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 4, 4, 4,
-	4, 4, 4, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 4, 4, 4, 4, 4,
-	4, 4, 4, 4, 4, 1, 1, 1, 1, 1, 1, 1, 1, 4, 4, 4, 4, 4, 4, 4];
+
+var trackGrid =
+	[4, 4, 4, 4, 4, 4, 1, 1, 1, 1, 1, 1, 1, 4, 4, 4, 4, 4, 4, 4,
+		4, 4, 1, 1, 1, 5, 0, 0, 0, 0, 0, 0, 0, 1, 1, 4, 4, 4, 4, 4,
+		4, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 4, 4, 4,
+		4, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 4, 4,
+		1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 4,
+		1, 0, 0, 0, 0, 5, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1,
+		1, 2, 2, 0, 5, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 0, 0, 0, 1, 1,
+		1, 1, 1, 1, 1, 1, 4, 4, 4, 4, 4, 4, 5, 5, 0, 0, 0, 0, 0, 1,
+		1, 0, 3, 0, 0, 0, 5, 4, 4, 4, 4, 5, 0, 0, 0, 0, 0, 0, 0, 1,
+		1, 0, 0, 0, 0, 0, 0, 5, 1, 1, 5, 0, 0, 0, 0, 0, 0, 0, 1, 1,
+		1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 1, 4,
+		4, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 4, 4,
+		4, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 4, 4, 4,
+		4, 4, 4, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 4, 4, 4, 4, 4,
+		4, 4, 4, 4, 4, 1, 1, 1, 1, 1, 1, 1, 1, 4, 4, 4, 4, 4, 4, 4];
 
 const TRACK_ROAD = 0;
 const TRACK_WALL = 1;
@@ -45,13 +45,13 @@ const TRACK_EDGE = 5;
 
 
 
-function isObsticalAtColRow(col, row) {
+function returnTileTypeAtColRow(col, row) {
 	if (col >= 0 && col < TRACK_COLS &&
 		row >= 0 && row < TRACK_ROWS) {
 		var trackIndexUnderCoord = rowColToArrayIndex(col, row);
-		return (trackGrid[trackIndexUnderCoord] != TRACK_ROAD);
+		return trackGrid[trackIndexUnderCoord];
 	} else {
-		return false;
+		return TRACK_WALL;
 	}
 }
 
@@ -62,12 +62,15 @@ function carTrackHandling(whichCar) {
 
 	if (carTrackCol >= 0 && carTrackCol < TRACK_COLS &&
 		carTrackRow >= 0 && carTrackRow < TRACK_ROWS) {
-
-		if (isObsticalAtColRow(carTrackCol, carTrackRow)) {
-			whichCar.x -= Math.cos(whichCar.ang)* whichCar.speed;
-			whichCar.y -= Math.sin(whichCar.ang)* whichCar.speed;
-
+		var tileHere = returnTileTypeAtColRow(carTrackCol, carTrackRow);
+		if (tileHere == TRACK_GOAL) {
+			whichCar.speed = 0;
+		}
+		else if (tileHere != TRACK_ROAD) {
+			whichCar.x -= Math.cos(whichCar.ang) * whichCar.speed;
+			whichCar.y -= Math.sin(whichCar.ang) * whichCar.speed;
 			whichCar.speed *= -.5;
+
 
 		} // end of track found
 	} // end of valid col and row
@@ -83,14 +86,14 @@ function drawTracks() {
 	var arrayIndex = 0;
 	var drawTileX = 0;
 	var drawTileY = 0;
-	for(var eachRow=0;eachRow<TRACK_ROWS;eachRow++) {
-		for(var eachCol=0;eachCol<TRACK_COLS;eachCol++) {
+	for (var eachRow = 0; eachRow < TRACK_ROWS; eachRow++) {
+		for (var eachCol = 0; eachCol < TRACK_COLS; eachCol++) {
 
-			var arrayIndex = rowColToArrayIndex(eachCol, eachRow); 
+			var arrayIndex = rowColToArrayIndex(eachCol, eachRow);
 			var tileKindHere = trackGrid[arrayIndex];
 			var useImg = trackPics[tileKindHere];
 
-			canvasContext.drawImage(useImg,drawTileX,drawTileY);
+			canvasContext.drawImage(useImg, drawTileX, drawTileY);
 			drawTileX += TRACK_W;
 			arrayIndex++;
 		} // end of for each col
