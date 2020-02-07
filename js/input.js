d0b1888e3015a4b8e86e8a3a7b359325706ef806
elijah-atkins <elijahorama@gmail.com>
2020-02-07 14:50:14 -0700

    player2 controls working

--- js/input.js
+++ js/input.js
@@ -3,6 +3,11 @@ const KEY_UP_ARROW = 38;
 const KEY_RIGHT_ARROW = 39;
 const KEY_DOWN_ARROW = 40;
 
+const KEY_W = 87;
+const KEY_A = 65;
+const KEY_S = 83;
+const KEY_D = 68;
+
 var mouseX = 0;
 var mouseY = 0;
 
@@ -12,7 +17,7 @@ function setupInput(){
 	document.addEventListener('keydown', keyPressed);
 	document.addEventListener('keyup', keyReleased);
 	blueCar.setupInput(KEY_UP_ARROW, KEY_RIGHT_ARROW, KEY_DOWN_ARROW,KEY_LEFT_ARROW)
-	redCar.setupInput(KEY_UP_ARROW, KEY_RIGHT_ARROW, KEY_DOWN_ARROW,KEY_LEFT_ARROW)
+	redCar.setupInput(KEY_W, KEY_D, KEY_S,KEY_A)
 
 }
 
@@ -25,35 +30,23 @@ function updateMousePos(evt) {
     
 }
 
-// function keySet(keyEvent, whichCar, setTo){
-// 	switch(keyEvent.keyCode){
-// 		case whichCar.controlKeyLeft:
-// 			whichCar.keyHeld_TurnLeft = setTo;
-// 			break;
-// 		case whichCar.controlKeyRight:
-// 			whichCar.keyHeld_TurnRight = setTo;
-// 			break;
-// 		case whichCar.controlKeyUp:
-// 			whichCar.keyHeld_Gas = setTo;
-// 			break;
-// 		case whichCar.controlKeyDown:
-// 			whichCar.keyHeld_Reverse = setTo;
-// 			break;
-// }
-function keySet(keyEvent, whichCar, setTo) {
-	if(keyEvent.keyCode == whichCar.controlKeyLeft) {
-		whichCar.keyHeld_TurnLeft = setTo;
-	}
-	if(keyEvent.keyCode == whichCar.controlKeyRight) {
-		whichCar.keyHeld_TurnRight = setTo;
-	}
-	if(keyEvent.keyCode == whichCar.controlKeyUp) {
-		whichCar.keyHeld_Gas = setTo;
-	}
-	if(keyEvent.keyCode == whichCar.controlKeyDown) {
-		whichCar.keyHeld_Reverse = setTo;
+function keySet(keyEvent, whichCar, setTo){
+	switch(keyEvent.keyCode){
+		case whichCar.controlKeyLeft:
+			whichCar.keyHeld_TurnLeft = setTo;
+			break;
+		case whichCar.controlKeyRight:
+			whichCar.keyHeld_TurnRight = setTo;
+			break;
+		case whichCar.controlKeyUp:
+			whichCar.keyHeld_Gas = setTo;
+			break;
+		case whichCar.controlKeyDown:
+			whichCar.keyHeld_Reverse = setTo;
+			break;
 	}
 }
+
 function keyPressed(evt){
 	keySet(evt, blueCar,true);
 	keySet(evt, redCar,true);
